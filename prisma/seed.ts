7c654c135383d5a23c11ab605d07284f791a689e
andumpangestu <andumpangestu12345@gmail.com>
2025-08-13 09:47:56 +0700

    feat: update seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,69 +1,62 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import bcrypt from 'bcryptjs';
-import { seedAreas } from './seeders/area-seeder';
-import { seedMachines } from './seeders/machine-seeder';
-import { seedRacks } from './seeders/rack-seeder';
-import { seedKanbans1 } from './seeders/kanban-1-seeder';
-import { seedKanbans2 } from './seeders/kanban-2-seeder';
-import { seedKanbans3 } from './seeders/kanban-3-seeder';
-import { seedKanbans4 } from './seeders/kanban-4-seeder';
-import { seedSuppliers } from './seeders/supplier-seeder';
-import { seedMakers } from './seeders/maker-seeder';
-
+import bcrypt from "bcryptjs";
+import { seedAreas } from "./seeders/area-seeder";
+import { seedMachines } from "./seeders/machine-seeder";
+import { seedRacks } from "./seeders/rack-seeder";
+import { seedKanbans1 } from "./seeders/kanban-1-seeder";
+import { seedKanbans2 } from "./seeders/kanban-2-seeder";
+import { seedKanbans3 } from "./seeders/kanban-3-seeder";
+import { seedKanbans4 } from "./seeders/kanban-4-seeder";
+import { seedSuppliers } from "./seeders/supplier-seeder";
+import { seedMakers } from "./seeders/maker-seeder";
 
 async function main() {
-    await prisma.user.createMany({
-        data: [
-            {
-                username: 'admin',
-                name: 'Admin',
-                role: 'admin',
-                password: await bcrypt.hash('123123', 10),
-            },
-            {
-                username: 'john',
-                name: 'John',
-                role: 'operator',
-                password: await bcrypt.hash('123123', 10),
-            },
-            {
-                username: 'asep',
-                name: 'Asep',
-                role: 'operator',
-                password: await bcrypt.hash('123123', 10),
-            },
-            {
-                username: 'lennon',
-                name: 'Lennon',
-                role: 'pic',
-                password: await bcrypt.hash('123123', 10),
-            },
+  await prisma.user.createMany({
+    data: [
+      {
+        username: "admin",
+        name: "Admin",
+        role: "admin",
+        password: await bcrypt.hash("password", 10),
+      },
+      {
+        username: "john",
+        name: "John",
+        role: "operator",
+        password: await bcrypt.hash("password", 10),
+      },
+      {
+        username: "asep",
+        name: "Asep",
+        role: "operator",
+        password: await bcrypt.hash("password", 10),
+      },
+      {
+        username: "lennon",
+        name: "Lennon",
+        role: "ppic",
+        password: await bcrypt.hash("password", 10),
+      },
+    ],
+  });
 
-        ],
-    });
-
-
-    await seedSuppliers();
-    await seedMakers();
-    await seedAreas();
-    await seedMachines();
-    await seedRacks();
-    await seedKanbans1();
-    await seedKanbans2();
-    await seedKanbans3();
-    await seedKanbans4();
-
+  await seedSuppliers();
+  await seedMakers();
+  await seedAreas();
+  await seedMachines();
+  await seedRacks();
+  await seedKanbans1();
+  await seedKanbans2();
+  await seedKanbans3();
+  await seedKanbans4();
 }
 
-
-
-
 main()
-    .catch((e) => {
-        console.error(e.message);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e.message);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
